a3809e1605bd1726211c0d470187d3c689d60466
yellowkk <yellowk4@hunet.co.kr>
2023-12-19 19:01:23 +0900

    firebase 연결

--- star-wars-project/src/App.js
+++ star-wars-project/src/App.js
@@ -9,16 +9,23 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
+  // useCallback을 사용하면 함수가 새로 생성되는 것을 방지할 수 있다.
   const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
+    // async/await로 작성시 try/catch문으로 에러처리
     try {
-      const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch(
+        'https://react-mini-project-658c7-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json'
+      ); //https://swapi.dev/api/films/
       if (!response.ok) {
         throw new Error('Something went wrong!');
       }
 
       const data = await response.json();
+
+      // 내일 할 차례
+      const loadedMovies = [];
 
       const transformedMovies = data.results.map((movieData) => {
         return {
@@ -37,10 +44,22 @@ function App() {
 
   useEffect(() => {
     fetchMoviesHandler();
-  }, [fetchMoviesHandler]);
+  }, [fetchMoviesHandler]); // 컴포넌트가 재 렌더링 될 때마다 fetchMoviesHandler가 새로 생성된다.
 
-  function addMovieHandler(movie) {
+  //try catch문으로 에러처리 생략
+  async function addMovieHandler(movie) {
     console.log(movie);
+
+    const response = await fetch(
+      'https://react-mini-project-658c7-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(movie),
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+    const data = await response.json();
+    console.log('data : ', data);
   }
 
   let content = <p>Found no movies.</p>;
--- star-wars-project/src/components/AddMovie.js
+++ star-wars-project/src/components/AddMovie.js
@@ -18,22 +18,22 @@ function AddMovie(props) {
       releaseDate: releaseDateRef.current.value,
     };
 
-    props.onAddMovie(movie);
+    props.onAddMovie(movie); // App.js의 addMovieHandler()를 호출한다.
   }
 
   return (
     <form onSubmit={submitHandler}>
       <div className={classes.control}>
-        <label htmlFor='title'>Title</label>
-        <input type='text' id='title' ref={titleRef} />
+        <label htmlFor="title">Title</label>
+        <input type="text" id="title" ref={titleRef} />
       </div>
       <div className={classes.control}>
-        <label htmlFor='opening-text'>Opening Text</label>
-        <textarea rows='5' id='opening-text' ref={openingTextRef}></textarea>
+        <label htmlFor="opening-text">Opening Text</label>
+        <textarea rows="5" id="opening-text" ref={openingTextRef}></textarea>
       </div>
       <div className={classes.control}>
-        <label htmlFor='date'>Release Date</label>
-        <input type='text' id='date' ref={releaseDateRef} />
+        <label htmlFor="date">Release Date</label>
+        <input type="text" id="date" ref={releaseDateRef} />
       </div>
       <button>Add Movie</button>
     </form>
