c8e85baf1acf0b4f3599148819ecc6e43551088b
yellowkk <yellowk4@hunet.co.kr>
2024-02-27 17:31:08 +0900

    리덕스

--- 19_redux_basics/src/components/Counter.js
+++ 19_redux_basics/src/components/Counter.js
@@ -1,12 +1,37 @@
+import { useSelector, useDispatch } from 'react-redux'; //react-redux팀이 제공하는 useSelector 함수를 사용하여 상태를 가져올 수 있습니다. useStore도 사용 가능
+
 import classes from './Counter.module.css';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const dispatch = useDispatch(); //useDispatch 함수를 사용하여 액션을 보낼 수 있습니다. 이 함수는 리덕스 스토어의 dispatch 메서드를 반환합니다.
+  const counter = useSelector((state) => state.counter); //useSelector 함수를 사용하여 상태를 가져올 수 있습니다. 이 함수는 리덕스 스토어의 상태를 가져오는 데 사용됩니다.
+  const show = useSelector((state) => state.showCounter);
+
+  const incrementHandler = () => {
+    dispatch({ type: 'increment' });
+  };
+
+  const increaseHandler = () => {
+    dispatch({ type: 'increase', amount: 5 });
+  };
+
+  const decrementHandler = () => {
+    dispatch({ type: 'decrement' });
+  };
+
+  const toggleCounterHandler = () => {
+    dispatch({ type: 'toggle' });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      {show && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- 19_redux_basics/src/store/index.js
+++ 19_redux_basics/src/store/index.js
@@ -1,15 +1,33 @@
 import { createStore } from 'redux';
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCounter: true };
+
+const counterReducer = (state = initialState, action) => {
   if (action.type === 'increment') {
     return {
       counter: state.counter + 1,
+      showCounter: state.showCounter,
+    };
+  }
+
+  if (action.type === 'increase') {
+    return {
+      counter: state.counter + action.amount,
+      showCounter: state.showCounter,
     };
   }
 
   if (action.type === 'decrement') {
     return {
       counter: state.counter - 1,
+      showCounter: state.showCounter,
+    };
+  }
+
+  if (action.type === 'toggle') {
+    return {
+      showCounter: !state.showCounter,
+      counter: state.counter,
     };
   }
 
@@ -19,3 +37,5 @@ const counterReducer = (state = { counter: 0 }, action) => {
 const store = createStore(counterReducer); // createStore에는 포인터가 들어가야 함, 리듀서 함수를 포인트 합니다.
 // createStore 취소선 관련해서는 https://redux.js.org/api/createstore 참고
 // Redux Toolkit이라는 라이브러리가 소개되면서, 이 라이브러리의 configureStore 함수로 대체 가능
+
+export default store;
