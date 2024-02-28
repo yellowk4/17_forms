69a709b71986ccef6b9a12ef6eb6a44ac4201bad
yellowkk <yellowk4@hunet.co.kr>
2024-02-28 14:48:34 +0900

    리덕스

--- 19_redux_basics/src/components/Counter.js
+++ 19_redux_basics/src/components/Counter.js
@@ -1,5 +1,5 @@
 import { useSelector, useDispatch } from 'react-redux'; //react-redux팀이 제공하는 useSelector 함수를 사용하여 상태를 가져올 수 있습니다. useStore도 사용 가능
-
+import { counterActions } from '../store/index';
 import classes from './Counter.module.css';
 
 const Counter = () => {
@@ -8,19 +8,23 @@ const Counter = () => {
   const show = useSelector((state) => state.showCounter);
 
   const incrementHandler = () => {
-    dispatch({ type: 'increment' });
+    // dispatch({ type: 'increment' });
+    dispatch(counterActions.increment());
   };
 
   const increaseHandler = () => {
-    dispatch({ type: 'increase', amount: 5 });
+    // dispatch({ type: 'increase', amount: 5 });
+    dispatch(counterActions.increase(5));
   };
 
   const decrementHandler = () => {
-    dispatch({ type: 'decrement' });
+    // dispatch({ type: 'decrement' });
+    dispatch(counterActions.decrement());
   };
 
   const toggleCounterHandler = () => {
-    dispatch({ type: 'toggle' });
+    // dispatch({ type: 'toggle' });
+    dispatch(counterActions.toggle());
   };
 
   return (
