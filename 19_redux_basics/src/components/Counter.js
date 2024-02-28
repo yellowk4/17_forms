46f59b9ce58b09bd2011296e27b12d691a3777f5
yellowkk <yellowk4@hunet.co.kr>
2024-02-28 19:04:37 +0900

    리덕스, 리덕스 툴킷 마무리

--- 19_redux_basics/src/components/Counter.js
+++ 19_redux_basics/src/components/Counter.js
@@ -1,11 +1,13 @@
 import { useSelector, useDispatch } from 'react-redux'; //react-redux팀이 제공하는 useSelector 함수를 사용하여 상태를 가져올 수 있습니다. useStore도 사용 가능
-import { counterActions } from '../store/index';
+// useSelector 함수로 초기 상태값을 등록
+// useDispatch 액션을 발생시켜서 상태값을 변경해 줍니다!
+import { counterActions } from '../store/counter';
 import classes from './Counter.module.css';
 
 const Counter = () => {
   const dispatch = useDispatch(); //useDispatch 함수를 사용하여 액션을 보낼 수 있습니다. 이 함수는 리덕스 스토어의 dispatch 메서드를 반환합니다.
-  const counter = useSelector((state) => state.counter); //useSelector 함수를 사용하여 상태를 가져올 수 있습니다. 이 함수는 리덕스 스토어의 상태를 가져오는 데 사용됩니다.
-  const show = useSelector((state) => state.showCounter);
+  const counter = useSelector((state) => state.counter.counter); //useSelector 함수를 사용하여 상태를 가져올 수 있습니다. 이 함수는 리덕스 스토어의 상태를 가져오는 데 사용됩니다. state.counter.counter는 counterSlice.reducer의 counter 키를 의미합니다.
+  const show = useSelector((state) => state.counter.showCounter);
 
   const incrementHandler = () => {
     // dispatch({ type: 'increment' });
